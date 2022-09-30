381f677a07348a3892749d6efc51430a5c94ecec
unknown <fboy4069@gmail.com>
2022-09-30 12:23:26 +0600

    changes on appointment form

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Appointment = () => {
 
@@ -8,7 +9,7 @@ const Appointment = () => {
     const onSubmit = data => {
 
 
-        const url = `https://manufacture-tribe.herokuapp.com/review`;
+        const url = `http://localhost:5000/appointment`;
         fetch(url, {
             method: 'POST',
             headers: {
@@ -34,15 +35,15 @@ const Appointment = () => {
 
                 <div className=''>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <input className='mb-2 appearance-none rounded-none relative block w-96 px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write your name' type='text' {...register("name")} />
-                        <input className='mb-2 appearance-none rounded-none relative block w-96 px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write your email' type='email' {...register("email")} required />
-                        <input className='mb-2  appearance-none rounded-none relative block w-96 px-5 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write your Phone(+91....)' type='text' {...register("phoneNumber")} />
+                        <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900 ' placeholder='Write your name' type='text' {...register("name")} />
+                        <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Write your email' type='email' {...register("email")} required />
+                        <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Write your Phone(+91....)' type='text' {...register("phoneNumber")} />
 
-                        <input className='mb-2 appearance-none rounded-none relative block w-96 px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Select the date ' type="date" {...register("date")} required />
-                        <textarea className='mb-2 appearance-none rounded-none relative block w-96 px-3 py-2 border border-slate-500  text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm' placeholder='Write a note' type="date" {...register("note")} />
+                        <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Select the date ' type="date" {...register("date")} required />
+                        <textarea className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Write a note' type="date" {...register("note")} />
 
                         <div className='flex justify-center mt-4'>
-                            <input className=' bg-violet-600 rounded-md hover:bg-violet-700 font-semibold text-white block w-40 px-3 py-2' value='Book Appointment' type="submit" />
+                            <input className=' bg-violet-600 rounded-xl hover:bg-violet-700 font-semibold text-white block w-40 px-3 py-2' value='Book Appointment' type="submit" />
                         </div>
                     </form>
                 </div>
