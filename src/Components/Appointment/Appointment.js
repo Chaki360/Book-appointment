0e9d669ef9afc68010f15ae332be137f172000b4
unknown <fboy4069@gmail.com>
2022-09-30 12:38:24 +0600

    deployed on detlify

--- src/Components/Appointment/Appointment.js
+++ src/Components/Appointment/Appointment.js
@@ -33,7 +33,7 @@ const Appointment = () => {
             <h1 className='text-2xl mb-4 mt-5  text-center font-bold text-violet-800'>Book Your Appointment Today</h1>
             <section className='justify-center flex mt-10 justify-items-center'>
 
-                <div className=''>
+                <div className='bg-slate-200'>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900 ' placeholder='Write your name' type='text' {...register("name")} />
                         <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Write your email' type='email' {...register("email")} required />
@@ -42,7 +42,7 @@ const Appointment = () => {
                         <input className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Select the date ' type="date" {...register("date")} required />
                         <textarea className='mb-2 appearance-none rounded-md relative block w-96 px-5 py-2 border border-slate-500  text-gray-900' placeholder='Write a note' type="date" {...register("note")} />
 
-                        <div className='flex justify-center mt-4'>
+                        <div className='flex justify-center mt-4 bg-slate-50'>
                             <input className=' bg-violet-600 rounded-xl hover:bg-violet-700 font-semibold text-white block w-40 px-3 py-2' value='Book Appointment' type="submit" />
                         </div>
                     </form>
